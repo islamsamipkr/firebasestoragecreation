2e6f9596533b990f8b5828555a67848f3c2ea717
Sami Islam <islam.samipkr@gmail.com>
2023-03-30 10:59:02 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 resource "google_storage_bucket" "default" {
   provider                    = google-beta
   name                        = "test_bucket"
-  location                    = "US"
+  location                    = "northamerica-northeast1"
+  project   = "avian-amulet-378416"
   uniform_bucket_level_access = true
 }
 
