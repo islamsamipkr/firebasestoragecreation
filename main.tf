84142a1f7387e6d41152945e8396797f51eb5f68
Sami Islam <islam.samipkr@gmail.com>
2023-03-30 10:58:24 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,12 @@
-provider "google-beta" {
-  user_project_override = true
+resource "google_storage_bucket" "default" {
+  provider                    = google-beta
+  name                        = "test_bucket"
+  location                    = "US"
+  uniform_bucket_level_access = true
 }
 
 resource "google_firebase_storage_bucket" "default" {
   provider  = google-beta
   project   = "avian-amulet-378416"
-  bucket_id = "gs://firebasestoragebucket"
+  bucket_id = google_storage_bucket.default.id
 }
