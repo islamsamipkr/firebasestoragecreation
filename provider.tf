3f5e6f8806aa7055814a22468db8e7e07c403ad6
Sami Islam <islam.samipkr@gmail.com>
2023-03-30 10:55:53 -0400

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,16 @@
+# Terraform configuration to set up providers by version.
+terraform {
+  required_providers {
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the provider to use a specific project, by default.
+# In this guide, this project is referred to as the "quota-check project".
+# Including `user_project_override = true` is required if a `billing_project` is specified.
+provider "google-beta" {
+  user_project_override = true
+}
