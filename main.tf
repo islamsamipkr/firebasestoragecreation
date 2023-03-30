4558259542654020f46679ac5c359453d86eb62d
Sami Islam <islam.samipkr@gmail.com>
2023-03-30 12:18:06 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,3 +11,16 @@ resource "google_firebase_storage_bucket" "default" {
   project   = "avian-amulet-378416"
   bucket_id = google_storage_bucket.default.id
 }
+resource "google_storage_bucket" "default1" {
+  provider                    = google-beta
+  name                        = "cloud_storage_firebase_bucket_2_sami"
+  location                    = "northamerica-northeast1"
+  project   = "avian-amulet-378416"
+  uniform_bucket_level_access = true
+}
+
+resource "google_firebase_storage_bucket" "default1" {
+  provider  = google-beta
+  project   = "avian-amulet-378416"
+  bucket_id = google_storage_bucket.default1.id
+}
